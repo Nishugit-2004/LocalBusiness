0a326ee90bf1281f54380d42ef5dc42d4c5aaae4
Nishugit-2004 <mangalarajashekar1009@gmail.com>
2026-04-04 13:49:35 +0530

    apply localtunnel bypass globally

--- myapp/src/api.js
+++ myapp/src/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API_BASE_URL = (process.env.REACT_APP_API_URL || 'http://localhost:4000').replace(/\/$/, '');
+const API_BASE_URL = (process.env.REACT_APP_API_URL || 'https://thin-kings-cough.loca.lt').replace(/\/$/, '');
+
+// Guarantee every generic axios call universally injects the LocalTunnel Bypass
+axios.defaults.headers.common['Bypass-Tunnel-Reminder'] = 'true';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
